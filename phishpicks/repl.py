17f1d31e65ca574f52344ab3a74d6e1116db86a1
Richard Podkolinski <devlar@gmail.com>
2024-08-19 09:10:22 +0200

    Configuration Flow in REPL

--- phishpicks/repl.py
+++ phishpicks/repl.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 import re
 from datetime import datetime
 from typing import Callable, Iterable, Any
-from phishpicks import Configuration, PhishPicks
+from phishpicks import Configuration, PhishData, PhishPicks
 from pydantic import BaseModel
 from prompt_toolkit.completion import WordCompleter, Completer, CompleteEvent, Completion
 from prompt_toolkit.completion import Completer
@@ -14,6 +14,174 @@ from prompt_toolkit.key_binding import KeyBindings, KeyPress
 from prompt_toolkit.formatted_text import HTML
 from prompt_toolkit.styles import Style
 from prompt_toolkit import prompt
+import pdb
+
+# @TODO: Add ctrl backspace
+# @TODO: Add ctrl arrows
+kb = KeyBindings()
+keys = []
+session = PromptSession()
+
+
+@kb.add('backspace')
+def up_menu(event):
+    buffer = event.current_buffer
+    if buffer.text == "" and buffer.cursor_position == 0:
+        raise KeyboardInterrupt
+    else:
+        buffer.delete_before_cursor(count=1)
+
+
+def main():
+    conf = configuration_flow()
+    pd = PhishData(config=conf)
+
+
+def configuration_flow() -> Configuration:
+    try:
+        conf = Configuration.from_json()
+    except FileNotFoundError:
+        print("\nConfiguration Not Found")
+        conf = configuration_prompts()
+        if conf:
+            conf.configure()
+    return conf
+
+
+def configuration_prompts() -> Configuration:
+    # print("Current Configuration")
+    # for key, value in self.config.dict().items():
+    #     print(f"{key!s:>25}: {value}")
+    # print("\n")
+
+    # What is the path to your phish folder?
+    # [0] "Z://Music//Phish"
+    # OR Manually enter a phish_folder
+    print("Starting Configuration Wizard\n")
+    print(f"Phish Folder:\n[0]: {Configuration.model_fields['phish_folder'].default}\n")
+    print("OR Manually enter a phish_folder")
+    prompt_text = HTML('<style color="#FFDC00">phishpicks > configuration > phish_folder > </style>')
+    user_input = session.prompt(prompt_text, key_bindings=kb)
+    if user_input in ['', '0']:
+        phish_folder = Configuration.model_fields['phish_folder'].default
+    else:
+        phish_folder = user_input
+    print(f"Phish Folder set to {phish_folder}")
+    print("\n")
+
+    # What is the format of your show folders?
+    # Dates MUST be in posix format so YYYY-MM-DD, e.g. 2025-01-01 etc.
+    # [0] Phish [0-9]* will match "Phish 2024-01-01 Some venue" style folders
+    # [1] [0-9]* will match "2024-01-01 Some venue" style folders
+    # OR Manually enter a show_glob and a venue_regex
+    # Set the show_glob AND the venue_regex
+    # Check and print the number of folders after configuration
+    print(f"Show Folder Format")
+    print(f"[0] Phish [0-9]* will match 'Phish 2024-01-01 Some venue' style folders")
+    print(f"[1] [0-9]* will match '2024-01-01 Some venue' style folders")
+    print("OR Manually enter a show_glob")
+    prompt_text = HTML('<style color="#FFDC00">phishpicks > configuration > show_glob > </style>')
+    user_input = session.prompt(prompt_text, key_bindings=kb)
+    if user_input in ['', '0']:
+        show_glob = Configuration.model_fields['show_glob'].default
+        venue_regex = Configuration.model_fields['venue_regex'].default
+    elif user_input in ['1']:
+        show_glob = "[0-9]*"
+        venue_regex = r'\d\d\d\d-\d\d-\d\d (.*?.*)'
+    else:
+        show_glob = user_input
+        venue_regex = HTML('<style color="#FFDC00">phishpicks > configuration > venue_regex > </style>')
+    print(f"Show Glob Format set to: {show_glob}")
+    print("\n")
+
+    # What is your desired configuration folder?
+    # [0] "~/.phishpicks"
+    # OR Manually enter config_folder
+    print("Configuration Folder")
+    print(f"[0] {Configuration.model_fields['config_folder'].default}")
+    print("OR Manually enter a config_folder")
+    prompt_text = HTML('<style color="#FFDC00">phishpicks > configuration > config_folder > </style>')
+    user_input = session.prompt(prompt_text, key_bindings=kb)
+    if user_input in ['', '0']:
+        config_folder = Configuration.model_fields['config_folder'].default
+    else:
+        config_folder = user_input
+    print(f"Configuration Folder set to {config_folder}")
+    print("\n")
+
+    # What is your desired backups_folder?
+    # [0] "~/.phishpicks"
+    # [1] "~/.phishpicks_backups"
+    # OR Manually enter a backups_folder
+    print("Backups Folder")
+    print(f"[0] {Configuration.model_fields['backups_folder'].default}")
+    print(f"[1] ~/.phishpicks")
+    print("OR Manually enter a backups_folder")
+    prompt_text = HTML('<style color="#FFDC00">phishpicks > configuration > backups_folder > </style>')
+    user_input = session.prompt(prompt_text, key_bindings=kb)
+    if user_input in ['', '0']:
+        backups_folder = Configuration.model_fields['backups_folder'].default
+    elif user_input in ['1']:
+        backups_folder = "~/.phishpicks"
+    else:
+        backups_folder = user_input
+    print(f"Backups Folder set to {backups_folder}")
+    print("\n")
+
+    # What is your media_player_path
+    # [0] "C://Program Files (x86)//Winamp//winamp.exe"
+    # OR Manually enter a media_player_path
+    print("Media Player Path")
+    print(f"[0] {Configuration.model_fields['media_player_path'].default}")
+    print("OR Manually enter a media_player_path")
+    prompt_text = HTML('<style color="#FFDC00">phishpicks > configuration > media_player_path > </style>')
+    user_input = session.prompt(prompt_text, key_bindings=kb)
+    if user_input in ['', '0']:
+        media_player_path = Configuration.model_fields['media_player_path'].default
+    else:
+        media_player_path = user_input
+    print(f"Backups Folder set to {media_player_path}")
+    print("\n")
+
+    # Print all the attributes in Configuration
+    # Confirm?
+    # Yes / No
+    # Yes, save_to_json
+    # No, exit
+    print("Confirm these Configuration Settings?")
+    print(f"Phish Folder set to {phish_folder}")
+    print(f"Show Glob Format set to: {show_glob}")
+    print(f"Configuration Folder set to {config_folder}")
+    print(f"Backups Folder set to {backups_folder}")
+    print(f"Backups Folder set to {media_player_path}")
+    print("\n")
+    print("Confirm? [Y]")
+    prompt_text = HTML('<style color="#FFDC00">phishpicks > configuration > configuration > </style>')
+    user_input = session.prompt(prompt_text, key_bindings=kb)
+    if user_input in ['y', 'Y']:
+        print("Writing Configuration")
+        config = Configuration(config_folder=config_folder,
+                               backups_folder=backups_folder,
+                               phish_folder=phish_folder,
+                               media_player_path=media_player_path,
+                               show_glob=show_glob,
+                               venue_regex=venue_regex)
+        return config
+    else:
+        raise Exception('Unconfigured, exiting...')
+
+
+def main_menu(self):
+    menus = ['help', 'shows', 'tracks', 'random', 'play', 'clear', 'exit']
+    completer = WordCompleter(menus)
+    prompt_text = HTML('<style color="#FFDC00">phishpicks > </style>')
+    input = session.prompt(prompt_text,
+                           completer=completer,
+                           placeholder='',
+                           complete_while_typing=True,
+                           key_bindings=self.kb,
+                           )
+    print(input)
 
 
 class DateTrackCompleter(Completer):
@@ -73,7 +241,8 @@ class PhishMenu(str):
 
 
 class PhishREPL(BaseModel):
-    pick: PhishPicks
+    # config = Configuration = None
+    # pick: PhishPicks = None
     kb: Any = None
     keys: Any = None
     session: Any = None
@@ -87,23 +256,12 @@ class PhishREPL(BaseModel):
     def menu(self, value):
         self._menu = value
 
-    def model_post_init(self, __context: Any):
-        self.kb = KeyBindings()
-        self.keys = []
-
-        # @TODO: Add ctrl backspace
-        # @TODO: Add ctrl arrows
-
-        @self.kb.add('backspace')
-        def up_menu(event):
-            buffer = event.current_buffer
-            if buffer.text == "" and buffer.cursor_position == 0:
-                raise KeyboardInterrupt
-            else:
-                buffer.delete_before_cursor(count=1)
-
     @staticmethod
     def load(**kwargs) -> PhishREPL:
+        # @TODO Configuration Step Here
+        conf = Configuration()
+        is_configured = conf.is_configured()
+        # @TODO: Launch configuration wizard here
         pp = PhishPicks.load(**kwargs)
         return PhishREPL(pick=pp)
 
@@ -188,21 +346,6 @@ class PhishREPL(BaseModel):
             else:
                 self.pick.pick_track(show_date, track_name, exact=True)
 
-    def main_menu(self):
-        menus = ['help', 'shows', 'tracks', 'random', 'play', 'clear', 'exit']
-        completer = WordCompleter(menus)
-        prompt_text = HTML('<style color="#FFDC00">phishpicks > </style>')
-        input = self.session.prompt(prompt_text,
-                                    completer=completer,
-                                    placeholder='',
-                                    complete_while_typing=True,
-                                    key_bindings=self.kb,
-                                    )
-        if input in menus:
-            self.menu = input.strip()
-        else:
-            print(self.pick.picks)
-
     def generic_commands_run(self, user_input: str) -> str:
         if not user_input:
             print(self.pick.picks)
@@ -240,137 +383,11 @@ class PhishREPL(BaseModel):
         else:
             return None, select_statement
 
-    def configure(self):
-        # completer = DateTrackCompleter(date_completer, self.pick.db.tracks_from_date)
-        self.session = PromptSession()
-
-        print("Current Configuration")
-        for key, value in self.pick.config.dict().items():
-            print(f"{key!s:>25}: {value}")
-        print("\n")
-
-        # What is the path to your phish folder?
-        # [0] "Z://Music//Phish"
-        # OR Manually enter a phish_folder
-        print("Starting Configuration Wizard")
-        print("\n")
-        print(f"Phish Folder:\n[0]: {Configuration.model_fields['phish_folder'].default}\n")
-        print("OR Manually enter a phish_folder")
-        prompt_text = HTML('<style color="#FFDC00">phishpicks > configuration > phish_folder > </style>')
-        user_input = self.session.prompt(prompt_text, key_bindings=self.kb)
-        if user_input in ['', '0']:
-            phish_folder = Configuration.model_fields['phish_folder'].default
-        else:
-            phish_folder = user_input
-        print(f"Phish Folder set to {phish_folder}")
-        print("\n")
-
-        # What is the format of your show folders?
-        # Dates MUST be in posix format so YYYY-MM-DD, e.g. 2025-01-01 etc.
-        # [0] Phish [0-9]* will match "Phish 2024-01-01 Some venue" style folders
-        # [1] [0-9]* will match "2024-01-01 Some venue" style folders
-        # OR Manually enter a show_glob and a venue_regex
-        # Set the show_glob AND the venue_regex
-        # Check and print the number of folders after configuration
-        print(f"Show Folder Format")
-        print(f"[0] Phish [0-9]* will match 'Phish 2024-01-01 Some venue' style folders")
-        print(f"[1] [0-9]* will match '2024-01-01 Some venue' style folders")
-        print("OR Manually enter a show_glob")
-        prompt_text = HTML('<style color="#FFDC00">phishpicks > configuration > show_glob > </style>')
-        user_input = self.session.prompt(prompt_text, key_bindings=self.kb)
-        if user_input in ['', '0']:
-            show_glob = Configuration.model_fields['show_glob'].default
-            venue_regex = Configuration.model_fields['venue_regex'].default
-        elif user_input in ['1']:
-            show_glob = "[0-9]*"
-            venue_regex = r'\d\d\d\d-\d\d-\d\d (.*?.*)'
-        else:
-            show_glob = user_input
-            venue_regex = HTML('<style color="#FFDC00">phishpicks > configuration > venue_regex > </style>')
-        print(f"Show Glob Format set to: {show_glob}")
-        print("\n")
-
-        # What is your desired configuration folder?
-        # [0] "~/.phishpicks"
-        # OR Manually enter config_folder
-        print("Configuration Folder")
-        print(f"[0] {Configuration.model_fields['config_folder'].default}")
-        print("OR Manually enter a config_folder")
-        prompt_text = HTML('<style color="#FFDC00">phishpicks > configuration > config_folder > </style>')
-        user_input = self.session.prompt(prompt_text, key_bindings=self.kb)
-        if user_input in ['', '0']:
-            config_folder = Configuration.model_fields['config_folder'].default
-        else:
-            config_folder = user_input
-        print(f"Configuration Folder set to {config_folder}")
-        print("\n")
-
-        # What is your desired backups_folder?
-        # [0] "~/.phishpicks"
-        # [1] "~/.phishpicks_backups"
-        # OR Manually enter a backups_folder
-        print("Backups Folder")
-        print(f"[0] {Configuration.model_fields['backups_folder'].default}")
-        print(f"[1] ~/.phishpicks")
-        print("OR Manually enter a backups_folder")
-        prompt_text = HTML('<style color="#FFDC00">phishpicks > configuration > backups_folder > </style>')
-        user_input = self.session.prompt(prompt_text, key_bindings=self.kb)
-        if user_input in ['', '0']:
-            backups_folder = Configuration.model_fields['backups_folder'].default
-        elif user_input in ['1']:
-            backups_folder = "~/.phishpicks"
-        else:
-            backups_folder = user_input
-        print(f"Backups Folder set to {backups_folder}")
-        print("\n")
-
-        # What is your media_player_path
-        # [0] "C://Program Files (x86)//Winamp//winamp.exe"
-        # OR Manually enter a media_player_path
-        print("Media Player Path")
-        print(f"[0] {Configuration.model_fields['media_player_path'].default}")
-        print("OR Manually enter a media_player_path")
-        prompt_text = HTML('<style color="#FFDC00">phishpicks > configuration > media_player_path > </style>')
-        user_input = self.session.prompt(prompt_text, key_bindings=self.kb)
-        if user_input in ['', '0']:
-            media_player_path = Configuration.model_fields['media_player_path'].default
-        else:
-            media_player_path = user_input
-        print(f"Backups Folder set to {media_player_path}")
-        print("\n")
-
-        # Print all the attributes in Configuration
-        # Confirm?
-        # Yes / No
-        # Yes, save_to_json
-        # No, exit
-        print("Confirm these Configuration Settings?")
-        print(f"Phish Folder set to {phish_folder}")
-        print(f"Show Glob Format set to: {show_glob}")
-        print(f"Configuration Folder set to {config_folder}")
-        print(f"Backups Folder set to {backups_folder}")
-        print(f"Backups Folder set to {media_player_path}")
-        prompt_text = HTML('<style color="#FFDC00">phishpicks > configuration > configuration > </style>')
-        user_input = self.session.prompt(prompt_text, key_bindings=self.kb)
-        if user_input in ['y', '']:
-            print("Writing Configuration")
-            config = Configuration(config_folder=config_folder,
-                          backups_folder=backups_folder,
-                          phish_folder=phish_folder,
-                          media_player_path=media_player_path,
-                          show_glob=show_glob,
-                          venue_regex=venue_regex)
-            config.save_to_json()
-        else:
-            print("Unconfirmed, exiting...")
-
-
-
     def start(self):
         self.session = PromptSession()
         while True:
             try:
-                #@TODO: Check if configured, if not configured, run config
+                # @TODO: Check if configured, if not configured, run config
                 if self._menu == 'main':
                     self.main_menu()
                 elif self._menu == 'shows':
@@ -402,16 +419,8 @@ class PhishREPL(BaseModel):
                         self.menu = 'shows'
                 elif self._menu == 'exit':
                     print("Goodbye")  # @TODO: Witty Exit
-                    self.pick.db.backup_show_special()
-                    self.pick.db.backup_track_special()
-                    self.pick.db.backup_last_played()
+                    # self.pick.db.backup_all()
                     raise KeyboardInterrupt
             except KeyboardInterrupt:
                 print("Goodbye")
                 break
-
-
-def main():
-    c = PhishREPL.load()
-    # c.start()
-    c.configure()
